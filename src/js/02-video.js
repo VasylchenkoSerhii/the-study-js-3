0a02d6758d7cb21db179f00960948148fc7958ba
Vasylchenko Serhii <darnki@gmail.com>
2022-07-24 22:52:19 +0300

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframeRef = document.querySelector('#vimeo-player');
+const keyTime = "videoplayer-current-time";
+const player = new Player(iframeRef);
+
+console.log(localStorage.getItem(keyTime))
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem(keyTime, data.seconds);
+}, 1000));
+player.setCurrentTime(Number(localStorage.getItem(keyTime)));
+
 

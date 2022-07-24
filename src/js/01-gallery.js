28dbd03daea57c3a05a47280a6d8536c44910b47
Vasylchenko Serhii <darnki@gmail.com>
2022-07-24 19:45:53 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryWripperRef = document.querySelector('.gallery');
+const marKupItemsGallery = makeGalleryOfMarKupElements(galleryItems);
+galleryWripperRef.innerHTML = marKupItemsGallery;
 
-console.log(galleryItems);
+
+function makeGalleryOfMarKupElements(items) {
+    return items.map(({ preview, original, description }) => 
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`)
+    .join('');
+};
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});

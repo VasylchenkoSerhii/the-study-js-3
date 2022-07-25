4c006b97d037bf18f4d1c813d22ddbba50b98a8c
Vasylchenko Serhii <darnki@gmail.com>
2022-07-25 15:02:14 +0300

    start third task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,19 +3,19 @@ import throttle from 'lodash.throttle';
 
 const iframeRef = document.querySelector('#vimeo-player');
 const player = new Player(iframeRef);
-const LOCAL_KEY = 'videoplayer-current-time'
+const LOCAL_KEY1 = 'videoplayer-current-time'
 
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(e) {
-    localStorage.setItem(LOCAL_KEY, e.seconds)
+    localStorage.setItem(LOCAL_KEY1, e.seconds)
 };
 
 setCurrentTime();
 
 function setCurrentTime() {
-    const savedTime = localStorage.getItem(LOCAL_KEY);
+    const savedTime = localStorage.getItem(LOCAL_KEY1);
     if (savedTime) {
         player.setCurrentTime(savedTime)
     }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const LOCAL_KEY = "feedback-form-state"
+console.log(localStorage.getItem(LOCAL_KEY))
+formRef.addEventListener('input', onInput)
+addOnInput()
+
+function onInput(e) {
+    const { currentTarget: {elements: {message, email}} } = e;
+    localStorage.setItem(LOCAL_KEY, `{email: ${email.value}, message: ${message.value}}`)
+}
+function addOnInput() {
+    const localValue = localStorage.getItem(LOCAL_KEY);
+    if (localValue) {
+    }
+    
+}
